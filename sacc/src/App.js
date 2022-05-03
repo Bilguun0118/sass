aa2b627774ca4ad4264be7f3a6056dda77224ebe
Bilguun <bilguun@LEAP-03s-iMac.local>
2022-05-03 12:09:32 +0800

    sass

---
+++ sacc/src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="containers flex align-items justify-content flex-dirextion">
+      <button className='btn'>Primary</button>
+      <button className='btn'>Secondary</button>
+      <button className='btn'>Disabled</button>
+      <button className='btn'>Success</button>
+      <button className='btn'>Delete</button>
+    </div>
+  );
+}
+
+export default App;
